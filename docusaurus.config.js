37ff62ea13c700fcff38922ff43e7a190230e6d3
Joao Mendonça <joaomvm.2006@gmail.com>
2025-12-16 21:24:58 +0000

    teste logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Tabacaria.exe',
   tagline: 'Inova o sistema do teu negócio de tabaco!!!',
-  favicon: 'img/tabacariad.jpg',
+  favicon: 'logo.jpeg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
