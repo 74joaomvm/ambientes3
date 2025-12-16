6b5c9886b6c0366239f474c5ddf5e636660de9c9
José Martins <a89810@ualg.pt>
2025-12-16 16:03:42 +0000

    Alteracao do logo do website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Tabacaria.exe',
   tagline: 'Inova o sistema do teu negócio de tabaco!',
-  favicon: 'img/tabacaria_logo_resized.ico',
+  favicon: 'img/tabacaria.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
