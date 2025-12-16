17e5c17711fe880067fe1d506cac0b9d1699081a
Joao Mendonça <joaomvm.2006@gmail.com>
2025-12-16 15:24:42 +0000

    js corrigido

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import MarkdownPage from '@site/src/pages/markdown-page.md'; // importar o arquivo markdown
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero-primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className={clsx(styles.heroTitle, styles.centeredTitle)}>
+        <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero_subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial -- 5min 🕒
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -34,12 +34,13 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="Descrição do seu projeto"
     >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
+      <MarkdownPage /> {/* Aqui, o conteúdo markdown será renderizado */}
     </Layout>
   );
 }
