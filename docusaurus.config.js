be43e2858a12f96682cab7e1e7a6a93e2b2d7499
José Martins <a89810@ualg.pt>
2025-12-16 17:12:10 +0000

    Alteracao da tagline (resolver problema)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Tabacaria.exe',
-  tagline: 'Inova o sistema do teu negócio de tabaco!',
+  tagline: 'Inova o sistema do teu negócio de tabaco!!!',
   favicon: 'img/tabacaria.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
