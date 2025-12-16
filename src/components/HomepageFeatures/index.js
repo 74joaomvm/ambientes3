0009370d0fa9a2ac1f47b3b596d2f54099958715
José Martins <a89810@ualg.pt>
2025-12-16 20:10:27 +0000

    (teste)tentar adicionar imagem na pagina inicial

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Aplicação Intuitiva',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: '@site/static/img/aplicacao_intuitiva.png',
     description: (
       <>
         A nossa aplicação é fácil de usar e foi projetada para simplificar a gestão do seu negócio de tabaco. Controle tudo com alguns cliques, desde o inventário até as vendas, de forma eficiente e intuitiva.
